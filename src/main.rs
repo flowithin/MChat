5a8ca9ee59ea7dee46e6116689e52951d137bdfc
jtwuuuuu <1874216549@qq.com>
2024-12-25 20:14:39 -0800

    hashmap, server display

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,63 @@
-use std::fmt::format;
 use std::fs::{self};
 use std::io::{prelude::*, BufReader};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
 use std::thread;
 use std::time::Duration;
 use Server::ThreadPool;
-
-fn client(stream: Arc<Mutex<TcpStream>>, messages: Sender<&str>) -> () {
-    let _ = messages.send("hellow, I am here!\n");
-    let mut buffer = vec![0; 1024];
+use std::collections::HashMap;
+enum message{
+  Connected{ip:SocketAddr, stream:Arc<TcpStream>},
+  unConnected{ip:SocketAddr,stream:Arc<TcpStream>},
+  New_message{str:String},
+}
+fn client(stream: Arc<TcpStream>, messages: Sender<message>) -> () {
+    let ip :SocketAddr= stream.as_ref().peer_addr().unwrap();
+    let _ = messages.send(message::Connected{ip,stream:stream.clone()});
+    let mut buffer = vec![0; 10];
     loop {
-        let n = stream.lock().unwrap().read(&mut buffer).unwrap();
-
+        let n = stream.as_ref().read(&mut buffer).unwrap();
         //let ip = stream.peer_addr().unwrap();
-        let message_content = "usr : sent {}";
+        let message_content = format!("{:?}", buffer);
         if n > 0 {
-            messages.send(&message_content).map_err(|err| {
+            messages.send(message::New_message{str:message_content.to_string()}).map_err(|err| {
                 eprintln!("cannot send message ERROR : {err}");
             });
             //write into the Tcp(multi user)
             //but not functioning very well
-            stream.lock().unwrap().write(&mut buffer);
+            //stream.lock().unwrap().write(&mut buffer);
         } else {
-            println!("n == 0");//printed only when disconnected
-            break;
+        messages.send(message::unConnected{ip,stream:stream.clone()}).map_err(|err| {
+                eprintln!("cannot send message ERROR : {err}");
+            });
+      break;
         }
     }
 }
 
-fn Server(messages: Receiver<&str>) -> () {
-    //TODO: client hashmap
+fn Server(messages: Receiver<message>) -> () {
+    let mut clients = HashMap::new();
     loop {
-        if let Ok(message) = messages.recv() {
-            println!("{:?}", message);
+        match messages.recv().unwrap(){ 
+        message::Connected{ip, stream}=>{
+            clients.insert(ip, stream);
+            println!("{:?}, got fucking connected", ip.to_string());
         }
+      message::New_message{str}=>{
+        println!("new message: {}",str);
     }
+      message::unConnected{ip, stream}=>{
+            println!("{:?}, got fucking disconnected", ip.to_string());
+      clients.remove(&ip);
+        //stream.shutdown(Both);
+    }
+  }
+  }
 }
 
 fn main() {
-    let listener = TcpListener::bind("10.0.0.35:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:6969").unwrap();
     let thread_cnt = 4;
     let threadpool = ThreadPool::new(thread_cnt);
     let (sender, receiver) = channel();
@@ -50,7 +67,7 @@ fn main() {
         let stream = stream.unwrap();
         //thread::spawn(|| handle_connection(stream));
         let sender = sender.clone();
-        let stream = Arc::new(Mutex::new(stream));
+        let stream = Arc::new(stream);
         ThreadPool::execute(&threadpool, || client(stream, sender));
         //handle_connection(stream);
     }
